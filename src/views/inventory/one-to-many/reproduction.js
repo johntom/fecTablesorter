cea0d6527e221130e8c43bafcbf1f21dfeddfdad
John Tomaselli <jrt@gtz.com>
2019-07-27 09:51:39 -0400

    951

--- src/views/inventory/one-to-many/reproduction.js
+++ src/views/inventory/one-to-many/reproduction.js
@@ -5,8 +5,14 @@ import { DialogService } from 'aurelia-dialog';
 import { Prompt } from '../../../services/prompt';
 import { PromptForm } from '../promptForm';
 import { Promptrepro } from '../../prompt/promptRepro';
-import jsRapTable from '../../../../jslib/jsRapTable';
- 
+
+   import jsRapTable from '../../../../jslib/jsRapTable';
+
+import tablesorter from  '../../../../jslib/jquery.tablesorter.min';
+import tablesorterw from '../../../../jslib/jquery.tablesorter.widgets';
+import tablesorters from '../../../../jslib/widget-scroller';
+
+
 @inject(ApiService, ApplicationService, DialogService)
 export class Reproduction {
   heading = 'DataForm HEADER...';
@@ -41,17 +47,17 @@ export class Reproduction {
 
   }
 
-  attached() {
-    $(document).ready(function () {
-      $('#dtVerticalScrollExample').DataTable({
-        "scrollY": "200px",
-        "scrollCollapse": true,
-        "ordering": false,
-      });
-      // $('.dataTables_length').addClass('bs-select');
-    });
+  // attached() {
+  //   $(document).ready(function () {
+  //     $('#dtVerticalScrollExample').DataTable({
+  //       "scrollY": "200px",
+  //       "scrollCollapse": true,
+  //       "ordering": false,
+  //     });
+  //     // $('.dataTables_length').addClass('bs-select');
+  //   });
 
-  }
+  // }
 
   modal(item, index) {
 
@@ -153,22 +159,63 @@ export class Reproduction {
   }
    attached() {
     $(document).ready(function () {
-      $('#raptable').jsRapTable({
-        onSort: function (i, d) {
-          $('tbody').find('td').filter(function () {
-            return $(this).index() === i;
-          }).sortElements(function (a, b) {
-            if (i)
-              return $.text([a]).localeCompare($.text([b])) * (d ? -1 : 1);
-            else
-              return (parseInt($.text([a])) - parseInt($.text([b]))) * (d ? -1 : 1);
-          }, function () {
-            return this.parentNode;
-          });
-        },
-      });
+    //   $('#raptable').jsRapTable({
+    //     onSort: function (i, d) {
+    //       $('tbody').find('td').filter(function () {
+    //         return $(this).index() === i;
+    //       }).sortElements(function (a, b) {
+    //         if (i)
+    //           return $.text([a]).localeCompare($.text([b])) * (d ? -1 : 1);
+    //         else
+    //           return (parseInt($.text([a])) - parseInt($.text([b]))) * (d ? -1 : 1);
+    //       }, function () {
+    //         return this.parentNode;
+    //       });
+    //     },
+    //   });
 
-    })
+     // $('table').tablesorter({
+      $('raptable').tablesorter({
+        
+        theme: 'default',
+        //   headerTemplate : '{content} {icon}',
+        widgets: ['filter', 'scroller'],
+
+        widgetOptions: {
+          // This allows setting the number of fixed columns to add to the
+          // scroller
+          scroller_fixedColumns: 1,
+          // Set the height of the scroll window in pixels
+          scroller_height: 400,
+          // scroll tbody to top after sorting
+          scroller_upAfterSort: true,
+          // pop table header into view while scrolling up the page
+          scroller_jumpToHeader: true,
+          // Setting this to true will add a fixed overlay which can be used
+          // for styling; A class name of "tablesorter-scroller-fixed-panel"
+          // is added to the overlay.
+          scroller_addFixedOverlay: false,
+          // Set the width of the scroll bar in pixels; set to `null` to have
+          // the width calculated internally as it is dependent on the browser
+          scroll_barWidth: null,
+          // Set this to a class name to use when hovering over a fixed column
+          // row
+          scroller_rowHighlight: "hover"
+        
+      },
+        initialized: function (raptable) {
+          // Not an ideal solution to fix column alignment,
+          // but it works (for now)
+          $(raptable).resize();
+        }
+      });
+    
+    
+     })
+
+
+
+
   }
 
 }
